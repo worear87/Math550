a718806ee99ebd820fc19bf9391836887a8cca27
Wallace O'Rear <worear@gmail.com>
2018-09-04 13:20:00 -0400

    added in the plots,lsfit line, and confint for 2.1 and 2.2

--- Homework1.R
+++ Homework1.R
@@ -1,22 +1,28 @@
 #### homework 1 
-#### problems: ch2: 1,2,4,4,6
+#### problems: ch2: 1,2,3,4,6
 
 library(readr)
 
-setwd("C:/code/Math550")
+setwd("F:/code/Math550")
 
 
 ## 2.1
 playbill <- read.csv("Data/playbill.csv")
 playbill.lm <- lm(CurrentWeek ~ LastWeek, data = playbill)
+plot(x = playbill$CurrentWeek,y = playbill$LastWeek, pch=19)
 summary(playbill.lm)
+abline(lsfit(x=playbill$CurrentWeek,y=playbill$LastWeek))
 
+round(confint(playbill.lm),2)
 
 ## 2.2
 indicators <- read.csv("Data/indicators.txt", sep="")
 indicators.lm <- lm(PriceChange ~ LoanPaymentsOverdue, data=indicators)
+plot(indicators$PriceChange,indicators$LoanPaymentsOverdue,pch=19)
 summary(indicators.lm)
+abline(lsfit(indicators$PriceChange,indicators$LoanPaymentsOverdue))
 
+round(confint(indicators.lm),2)
 
 ## 2.3
 invoices <- read.csv("Data/invoices.txt", sep="")
